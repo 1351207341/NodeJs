b562eb3b9b2dfbb8cedfc5c6cc747e270ba47ca5
1351207341 <1351207341@qq.com>
2017-05-08 23:28:27 +0800

    modify find2.js

--- find2.js
+++ find2.js
@@ -1,3 +1,13 @@
 var mongoose = require('mongoose');
 
 require('./model.js');
+
+var User = mongoose.model('User');
+
+User.find({}, function(err, docs){
+  if(err){
+    console.log(err);
+    return;
+  }
+  console.log(docs);
+});
